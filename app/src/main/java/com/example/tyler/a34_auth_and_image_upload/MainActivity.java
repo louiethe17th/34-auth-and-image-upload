0a622f72ee324f0a42ed46e9d03ae5f11a54fb34
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-08-24 10:42:55 -0700

    scaffolded

---
+++ app/src/main/java/com/example/tyler/a34_auth_and_image_upload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tyler.a34_auth_and_image_upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
